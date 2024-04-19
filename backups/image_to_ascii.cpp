347479f8dc616f196e9c30a4fd7041d6b837348b
Terry L <yuxlin21@gmail.com>
2024-04-19 13:08:39 -0400

    replace

--- backups/image_to_ascii.cpp
+++ backups/image_to_ascii.cpp
@@ -6,36 +6,86 @@
 #include <cstdlib>
 #include <getopt.h>
 #include <opencv2/opencv.hpp>
-#include <filesystem>
+// #include <filesystem>
 #include <mpi.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <unistd.h>
 
-namespace fs = std::filesystem;
+// namespace fs = std::filesystem;
 
-int CHAR_WIDTH = 10;
-int CHAR_HEIGHT = 18;
+int CHARACTER_WIDTH = 10;
+int CHARACTER_HEIGHT = 18;
 float SCALE_FACTOR = 1.0;
 
-std::string characters = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
+// default characters used for ASCII art
+std::string characters = " .'`^\",:;Il!i><~+_-?][}{1)(|\\//tfjrxnuvczXYUJCLQ0OZmwqpbdkhao*#MWM&8%B@$";
 
+// Check if a directory exists
+bool directory_exists(const std::string &path)
+{
+    struct stat buffer;
+    return (stat(path.c_str(), &buffer) == 0);
+}
+
+// Create a directory
+bool create_directory(const std::string &path)
+{
+    return (mkdir(path.c_str(), 0777) == 0);
+}
+
+// Check if a file exists
+bool file_exists(const std::string &name)
+{
+    return (access(name.c_str(), F_OK) != -1);
+}
+
+// Remove a file
+bool remove_file(const std::string &name)
+{
+    return (remove(name.c_str()) == 0);
+}
+
+// Iterate over files in a directory
+void iterate_directory(const std::string &path)
+{
+    DIR *dir;
+    struct dirent *ent;
+    if ((dir = opendir(path.c_str())) != nullptr)
+    {
+        while ((ent = readdir(dir)) != nullptr)
+        {
+            std::string file_name = ent->d_name;
+            // process file_name
+        }
+        closedir(dir);
+    }
+}
+
+// reverse the characters used for ASCII art. This is used to get the negative of the ASCII image
 void reverse_string(std::string &str)
 {
     std::reverse(str.begin(), str.end());
 }
 
+// display the usage of the program
 void show_usage(const std::string &executable_name)
 {
     std::cerr << "\nUsage: " << executable_name << " -i <DIRECTORY> [options] \n\n";
     std::cerr << "Options:\n"
+                 "  -h, --help              Display this help message\n"
                  "  -i, --input  <DIRECTORY>     Specify the path of the input image directory (required)\n"
                  "  -w, --width  <NUMBER>   Set the width of the ASCII output; maintains aspect ratio\n"
                  "  -s, --chars  <STRING>   Define the set of characters used in the ASCII output\n"
                  "  -p, --print             Print the ASCII output to the console\n"
-                 "  -r, --reverse           Reverse the character set used for ASCII output(This option usually results in better PNG's)\n"
-                 "  -f, --factor            Set the scale factor to resize the image(0.0 - 1.0)\n"
+                 "  -n, --negate            Get the negative of the ASCII image\n"
+                 "  -f, --factor            Set the scale factor(a number from 0.0 to 1.0) to resize the image\n"
                  "  -c, --color             Get ASCII PNG's in colors\n\n";
+
     exit(EXIT_FAILURE);
 }
 
+// get the basename of a file, used to match the output file name with the input file name
 std::string get_basename(const std::string &full_path)
 {
     size_t last_slash = full_path.find_last_of("/\\");
@@ -48,20 +98,33 @@ std::string get_basename(const std::string &full_path)
     return basename;
 }
 
+// need to make sure we're writing to a new file and directory
 void check_output_directory()
 {
-    if (!fs::exists("outputs"))
+    // if (!fs::exists("outputs"))
+    // {
+    //     fs::create_directory("outputs");
+    // }
+
+    if (!directory_exists("outputs"))
     {
-        fs::create_directory("outputs");
+        create_directory("outputs");
     }
 
-    if (fs::exists("outputs/all_output.txt"))
+    // if (fs::exists("outputs/all_output.txt"))
+    // {
+    //     fs::remove("outputs/all_output.txt");
+    // }
+
+    // Instead of fs::remove
+    if (file_exists("outputs/all_output.txt"))
     {
-        fs::remove("outputs/all_output.txt");
+        remove_file("outputs/all_output.txt");
     }
 }
 
-void parse_arguments(int argc, char **argv, std::string &input_directory, std::string &executable_name, bool &resize_flag, int &desired_width, bool &print_flag, bool &reverse_flag, bool &colored_flag)
+// parse the command line arguments and set the configuration
+void parse_arguments(int argc, char **argv, std::string &input_directory, std::string &executable_name, bool &resize_flag, int &desired_width, bool &print_flag, bool &negate_flag, bool &colored_flag)
 {
     struct option long_options[] = {
         {"help", no_argument, nullptr, 'h'},
@@ -69,13 +132,13 @@ void parse_arguments(int argc, char **argv, std::string &input_directory, std::s
         {"width", required_argument, nullptr, 'w'},
         {"chars", required_argument, nullptr, 's'},
         {"print", no_argument, nullptr, 'p'},
-        {"reverse", no_argument, nullptr, 'r'},
+        {"negate", no_argument, nullptr, 'n'},
         {"factor", required_argument, nullptr, 'f'},
         {"color", no_argument, nullptr, 'c'},
         {0, 0, 0, 0}};
 
     int option;
-    const char *short_options = "hi:w:s:prcf:";
+    const char *short_options = "hi:w:s:pncf:";
     while ((option = getopt_long(argc, argv, short_options, long_options, nullptr)) != EOF)
     {
         switch (option)
@@ -96,8 +159,8 @@ void parse_arguments(int argc, char **argv, std::string &input_directory, std::s
         case 'p':
             print_flag = true;
             break;
-        case 'r':
-            reverse_flag = true;
+        case 'n':
+            negate_flag = true;
             break;
         case 'f':
             SCALE_FACTOR = std::atof(optarg);
@@ -111,6 +174,7 @@ void parse_arguments(int argc, char **argv, std::string &input_directory, std::s
     }
 }
 
+// load the image from the input directory
 cv::Mat load_image(const std::string &input_directory)
 {
     cv::Mat image = cv::imread(input_directory, cv::IMREAD_COLOR);
@@ -124,7 +188,8 @@ cv::Mat load_image(const std::string &input_directory)
     return image;
 }
 
-void resize_image(cv::Mat &image, int desired_width, int desired_height, bool resize_flag)
+// resize the image to the desired width and height
+void resize_image(cv::Mat &image, int &desired_width, int &desired_height, bool resize_flag)
 {
     if (resize_flag)
     {
@@ -136,19 +201,14 @@ void resize_image(cv::Mat &image, int desired_width, int desired_height, bool re
         desired_height = image.rows;
     }
 
-    cv::resize(image, image, cv::Size(static_cast<int>(desired_width * SCALE_FACTOR), static_cast<int>(desired_height * SCALE_FACTOR * (static_cast<float>(CHAR_WIDTH) / CHAR_HEIGHT))));
+    cv::resize(image, image, cv::Size(static_cast<int>(desired_width * SCALE_FACTOR), static_cast<int>(desired_height * SCALE_FACTOR * (static_cast<float>(CHARACTER_WIDTH) / CHARACTER_HEIGHT))));
 }
 
-void process_image(cv::Mat &image, const std::string &input_filepath, bool colored_flag, int desired_width, int desired_height, std::unique_ptr<std::string> &local_output)
+// process the image to get the ASCII art string and the ASCII image
+std::pair<std::string, cv::Mat> process_image(const cv::Mat &image, bool colored_flag)
 {
-    std::string output_filepath = get_basename(input_filepath);
-
-    std::ofstream file("outputs/" + output_filepath + ".txt");
-
-    file << "Input: " << input_filepath << "\nOutput: " << output_filepath
-         << ".txt\nResolution: " << desired_width << 'x' << desired_height << "\nCharacters (" << characters.size() << "): \"" << characters << "\"\n\n";
-
-    cv::Mat asciiImage(CHAR_HEIGHT * image.rows, CHAR_WIDTH * image.cols, image.type());
+    std::string asciiArt;
+    cv::Mat asciiImage(CHARACTER_HEIGHT * image.rows, CHARACTER_WIDTH * image.cols, image.type());
 
     for (int i = 0; i < image.rows; i++)
     {
@@ -156,28 +216,40 @@ void process_image(cv::Mat &image, const std::string &input_filepath, bool color
         {
             // Magic 🧙🏻🧙🏻‍♂️🧙🏻‍♀️
             cv::Vec3b pixel = image.at<cv::Vec3b>(i, j);
+
             float gray = 0.299 * pixel[0] + 0.587 * pixel[1] + 0.114 * pixel[2];
             int index = static_cast<int>(gray * (characters.size() - 1) / 255);
-
             char asciiChar = characters[index];
 
             cv::Scalar textColor = (colored_flag) ? cv::Scalar(pixel[0], pixel[1], pixel[2]) : cv::Scalar::all(255);
 
-            cv::putText(asciiImage, std::string(1, asciiChar), cv::Point(j * CHAR_WIDTH, i * CHAR_HEIGHT + CHAR_HEIGHT), cv::FONT_HERSHEY_SIMPLEX, 0.5, textColor, 1);
+            cv::putText(asciiImage, std::string(1, asciiChar), cv::Point(j * CHARACTER_WIDTH, i * CHARACTER_HEIGHT + CHARACTER_HEIGHT), cv::FONT_HERSHEY_SIMPLEX, 0.5, textColor, 1);
 
-            *local_output += asciiChar;
-            file << asciiChar;
+            asciiArt += asciiChar;
         }
 
-        *local_output += '\n';
-        file << '\n';
+        asciiArt += '\n';
     }
+
+    asciiArt += '\n';
+
+    return {asciiArt, asciiImage};
+}
+
+// save the ASCII art to a matching .txt file and a .png file
+void save_output(const std::string &asciiArt, const cv::Mat &asciiImage, const std::string &input_filepath, bool colored_flag, int desired_width, int desired_height)
+{
+    std::string output_filepath = get_basename(input_filepath);
+    std::ofstream file("outputs/" + output_filepath + ".txt");
+
+    file << "Input: " << input_filepath << "\nOutput: " << output_filepath
+         << ".txt\nResolution: " << desired_width << 'x' << desired_height
+         << "\nCharacters (" << characters.size() << "): \"" << characters << "\"\n\n"
+         << asciiArt;
 
     std::string color_output_string = (colored_flag) ? "_color" : "";
 
     cv::imwrite("outputs/" + output_filepath + color_output_string + ".png", asciiImage);
-
-    *local_output += "\nASCII art saved to '" + output_filepath + ".txt' and '" + output_filepath + color_output_string + ".png'\n\n";
 
     file.close();
 }
@@ -197,15 +269,15 @@ int main(int argc, char **argv)
     // Parse arguments from the command line
 
     std::string input_directory;
-    bool reverse_flag = false;
+    bool negate_flag = false;
     bool print_flag = false;
     bool colored_flag = false;
     bool resize_flag = false;
     int desired_width = 0;
 
-    parse_arguments(argc, argv, input_directory, executable_name, resize_flag, desired_width, print_flag, reverse_flag, colored_flag);
+    parse_arguments(argc, argv, input_directory, executable_name, resize_flag, desired_width, print_flag, negate_flag, colored_flag);
 
-    if (reverse_flag)
+    if (negate_flag)
     {
         reverse_string(characters);
     }
@@ -221,6 +293,7 @@ int main(int argc, char **argv)
 
     int desired_height = 0;
 
+    // MPI I/O
     MPI_File mpi_file;
     int err = MPI_File_open(MPI_COMM_WORLD, "outputs/all_output.txt",
                             MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &mpi_file);
@@ -252,16 +325,46 @@ int main(int argc, char **argv)
     // Ensure the header is written before other ranks start writing
     MPI_Barrier(MPI_COMM_WORLD);
 
+    // std::vector<std::string> input_files;
+
+    // for (const auto &entry : fs::directory_iterator(input_directory))
+    // {
+    //     std::string extension = entry.path().extension().string();
+    //     if (extension != ".jpg" && extension != ".jpeg" && extension != ".png")
+    //     {
+    //         continue;
+    //     }
+    //     input_files.push_back(entry.path().string());
+    // }
+
     std::vector<std::string> input_files;
 
-    for (const auto &entry : fs::directory_iterator(input_directory))
+    DIR *dir = opendir(input_directory.c_str());
+    if (dir != nullptr)
     {
-        std::string extension = entry.path().extension().string();
-        if (extension != ".jpg" && extension != ".jpeg" && extension != ".png")
+        struct dirent *entry;
+        while ((entry = readdir(dir)) != nullptr)
         {
-            continue;
+            // Check if the entry is a regular file
+            if (entry->d_type == DT_REG)
+            {
+                std::string file_name = entry->d_name;
+                // Extract file extension and convert to lower case for comparison
+                std::string extension;
+                size_t dot_pos = file_name.rfind('.');
+                if (dot_pos != std::string::npos && dot_pos + 1 < file_name.length())
+                {
+                    extension = file_name.substr(dot_pos);
+                    std::transform(extension.begin(), extension.end(), extension.begin(), ::tolower);
+                }
+                // Check if the file is an image based on the extension
+                if (extension == ".jpg" || extension == ".jpeg" || extension == ".png")
+                {
+                    input_files.push_back(input_directory + '/' + file_name);
+                }
+            }
         }
-        input_files.push_back(entry.path().string());
+        closedir(dir);
     }
 
     int num_files = input_files.size();
@@ -269,16 +372,19 @@ int main(int argc, char **argv)
     int start_index = myRank * files_per_rank;
     int end_index = (myRank + 1) * files_per_rank;
 
+    // Ensure the last rank processes any remaining files
     if (myRank == numRanks - 1)
-    { // Ensure the last rank processes any remaining files
+    {
         end_index = num_files;
     }
 
     if (start_index >= num_files)
     {
-        MPI_Barrier(MPI_COMM_WORLD); // Synchronize before any rank exits
+        // Synchronize before any rank exits
+        MPI_Barrier(MPI_COMM_WORLD);
         MPI_Finalize();
-        return EXIT_SUCCESS; // Exit if no work for this rank
+        // Exit if no work for this rank
+        return EXIT_SUCCESS;
     }
 
     // create local storage for each rank
@@ -292,7 +398,22 @@ int main(int argc, char **argv)
 
         resize_image(image, desired_width, desired_height, resize_flag);
 
-        process_image(image, input_filepath, colored_flag, desired_width, desired_height, local_output);
+        // Process the image to get ASCII art string and optionally adjust the image size again for ASCII dimensions
+        std::pair<std::string, cv::Mat> process_output = process_image(image, colored_flag);
+
+        std::string processed_string = process_output.first;
+
+        *local_output += processed_string;
+
+        cv::Mat processed_image = process_output.second;
+
+        // Save the ASCII art to a file and optionally save the colored ASCII image
+        save_output(processed_string, processed_image, input_filepath, colored_flag, desired_width, desired_height);
+
+        *local_output += "\nASCII art saved to '" + get_basename(input_filepath) + ".txt' and '" + get_basename(input_filepath) + "_color.png'\n\n";
+
+        image.release();
+        processed_image.release();
     }
 
     if (print_flag)
